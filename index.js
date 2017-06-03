5efe6fcaf668fe99dd57a708cde903e23c1a1366
zanjs <root@zanjs.com>
2017-06-03 22:59:56 +0800

    Every noble work is at first impossible :raising_hand:

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Koa = require('koa')
+const convert = require('koa-convert')
+const logger = require('./middleware/logger-generator')
+
 const app = new Koa()
+
+app.use(convert(logger()))
 
 app.use( async (ctx) =>{
     ctx.body = 'hello koa2'
