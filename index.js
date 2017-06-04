b1fa9df91e46d45fbb08d28980a3a70b20197970
zanjs <root@zanjs.com>
2017-06-04 19:13:29 +0800

    最简单的路由 :nose:

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const fs = require('fs')
 const convert = require('koa-convert')
 const logger = require('./middleware/logger-generator')
 
@@ -6,8 +7,58 @@ const app = new Koa()
 
 app.use(convert(logger()))
 
+/**
+ * 用Promise封装异步读取文件方法
+ * @param  {string} page html文件名称
+ * @return {promise} 
+ */
+function render(page){
+    return new Promise((resolve,reject)=>{
+        let viewUrl = `./view/${page}`
+
+        fs.readFile(viewUrl, "binary",(err,data)=>{
+
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+
+        })
+
+    })
+
+}
+
+async function route(url){
+
+    let view = '404.html'
+
+    switch(url){
+        case '/':
+            view = 'index.html'
+            break
+        case '/index':
+            view = 'index.html'
+             break
+        case '/todo':
+            view = 'todo.html'
+            break
+        case '/404':
+            view = '404.html'
+            break
+        default:
+            break
+    }
+    let html = await render( view )
+    return html
+}
+
+
 app.use( async (ctx) =>{
-    ctx.body = 'hello koa2'
+    let url = ctx.request.url
+    let html = await route( url )
+    ctx.body = html
 })
 
 app
